68dd8d4f9491bbd27e6c918122cea0a21aab29e4
Mitch Amiano <mcamiano@ncsu.edu>
2019-11-21 17:09:03 -0500

    Slinging more mud at the wall with a different babel config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app',
-  ],
-  env: {
-    test: {
-      presets: [['@babel/preset-env']],
-    },
-  },
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
 };
